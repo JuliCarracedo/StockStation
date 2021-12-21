ff81e9549bdadb1697147f9cc27a33934b5b874c
JuliCarracedo <julicarra@hotmail.com>
2021-12-21 10:18:21 -0300

    implement filter

--- src/components/CompanyLink.js
+++ src/components/CompanyLink.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+export const chooseSubtext = (filter, company) => {
+  switch (filter) {
+    case 'income': return `Income: ${company.netIncome}`;
+    case 'alphabetical': return `Income: ${company.netIncome}`;
+    case 'eps': return `EPS: ${company.eps}`;
+    case 'date': return `Date: ${company.date}`;
+    default: return `Income: ${company.netIncome}`;
+  }
+};
+
 const CompanyLink = (props) => {
-  const { title } = props;
-  return (<div className="company-link">{title}</div>);
+  const { company, filter } = props;
+  const subtext = chooseSubtext(filter, company);
+  return (
+    <div className="company-link">
+      <h3>{company.symbol}</h3>
+      <p>{subtext}</p>
+    </div>
+  );
 };
 
 CompanyLink.propTypes = {
-  title: PropTypes.string.isRequired,
+  company: PropTypes.objectOf(PropTypes.any).isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 export default CompanyLink;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,16 +3,25 @@ import { useSelector } from 'react-redux';
 import CompanyLink from '../CompanyLink';
 import './Home.css';
 
+export const filterList = (list, filter) => {
+  switch (filter) {
+    case 'income': return list.sort((a, b) => Number.parseFloat(a.income) - Number.parseFloat(b.income));
+    case 'eps': return list.sort((a, b) => Number.parseFloat(a.eps) - Number.parseFloat(b.eps));
+    case 'date': return list.sort((a, b) => new Date(a.date) - new Date(b.date));
+    case 'alphabetical': return list.sort((a, b) => ((a.symbol < b.symbol) ? -1 : 1));
+    default: return list;
+  }
+};
+
 const Home = () => {
   const { list } = useSelector((state) => state.stocks);
-  console.log(list, 'is the list');
   const [currentFilter, setFilter] = useState('default');
   const handleFilter = (e) => {
     e.stopPropagation();
     console.log(e.target.value, 'selected');
     setFilter(e.target.value);
   };
-  console.log(currentFilter, 'is current filter');
+  const filteredList = filterList(list, currentFilter);
   return (
     <div className="home-container">
       <div className="welcome-home">
@@ -27,8 +36,14 @@ const Home = () => {
         <option value="eps">EPS</option>
       </select>
       <div className="links">
-        {list.map(
-          (company) => <CompanyLink key={company.symbol} title={company.symbol} />,
+        {filteredList.map(
+          (company) => (
+            <CompanyLink
+              key={company.symbol}
+              company={company}
+              filter={currentFilter}
+            />
+          ),
         )}
       </div>
     </div>
